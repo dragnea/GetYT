103a2296cc7b7ec7d2cfda7ebfcaecd1eb937c49
Mihai Dragnea <mihaidragnea@mihais-mac-mini.local>
2015-11-04 14:04:19 +0200

    Added animation for Play button icon

--- GetYT/View/PlayButton.m
+++ GetYT/View/PlayButton.m
@@ -13,21 +13,37 @@
 - (void)setPaused:(BOOL)paused {
     _paused = paused;
     
-    if (_paused) {
-        [self setImage:[UIImage imageNamed:self.fullscreen ? @"control_pause_fullscreen" : @"control_pause_normal"] forState:UIControlStateNormal];
-    } else {
-        [self setImage:[UIImage imageNamed:self.fullscreen ? @"control_play_fullscreen" : @"control_play_normal"] forState:UIControlStateNormal];
-    }
+    [UIView transitionWithView:self
+                      duration:0.3
+                       options:UIViewAnimationOptionCurveEaseOut
+                    animations:^{
+                        
+                        if (_paused) {
+                            [self setImage:[UIImage imageNamed:self.fullscreen ? @"control_pause_fullscreen" : @"control_pause_normal"] forState:UIControlStateNormal];
+                        } else {
+                            [self setImage:[UIImage imageNamed:self.fullscreen ? @"control_play_fullscreen" : @"control_play_normal"] forState:UIControlStateNormal];
+                        }
+                        
+                    } completion:^(BOOL finished) {
+                        
+                    }];
 }
 
 - (void)setFullscreen:(BOOL)fullscreen {
     _fullscreen = fullscreen;
     
-    if (_fullscreen) {
-        [self setImage:[UIImage imageNamed:self.paused ? @"control_pause_fullscreen" : @"control_play_fullscreen"] forState:UIControlStateNormal];
-    } else {
-        [self setImage:[UIImage imageNamed:self.paused ? @"control_pause_normal" : @"control_play_normal"] forState:UIControlStateNormal];
-    }
+    [UIView transitionWithView:self
+                      duration:0.3
+                       options:UIViewAnimationOptionCurveEaseOut
+                    animations:^{
+                        if (_fullscreen) {
+                            [self setImage:[UIImage imageNamed:self.paused ? @"control_pause_fullscreen" : @"control_play_fullscreen"] forState:UIControlStateNormal];
+                        } else {
+                            [self setImage:[UIImage imageNamed:self.paused ? @"control_pause_normal" : @"control_play_normal"] forState:UIControlStateNormal];
+                        }
+                    } completion:^(BOOL finished) {
+                        
+                    }];
 }
 
 @end
